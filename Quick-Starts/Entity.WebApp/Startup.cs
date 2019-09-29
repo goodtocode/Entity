a46ddb266ba8565f40bbc88ce5c4b60d7854746f
Robert J. Good <robert.good@goodfamily.me>
2019-09-28 19:32:01 -0700

    Sprint 9 - IOption pattern implemented for HttpCrud and HttpQuery

--- Quick-Starts/Entity.WebApp/Startup.cs
+++ Quick-Starts/Entity.WebApp/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
 
 namespace GoodToCode.Entity.WebApp
 {
