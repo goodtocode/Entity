b5248fd32f4295332537b9dcd8d96d99104d35b0
goodtocode <robert.good@goodtocode.com>
2019-12-30 13:49:30 -0800

    Duplicate HttpGet route in PersonSearchController. Eliminated query parameters, only support path parameters.

--- Quick-Starts/Entity.WebServices/Controllers/PersonSearchController.cs
+++ Quick-Starts/Entity.WebServices/Controllers/PersonSearchController.cs
@@ -30,7 +30,7 @@ namespace GoodToCode.Entity.WebServices
         /// <param name="firstName">String - Text to search in first name</param>
         /// <param name="lastName">String - Text to search in the last name field</param>
         /// <returns>Partial view of only the search results region</returns>
-        [HttpGet(ControllerRoute + "/{idOrKey}/{firstName}/{lastName}"), Route(ControllerRoute)]
+        [HttpGet(ControllerRoute + "/{idOrKey}/{firstName}/{lastName}")]
         public IEnumerable<IPerson> Get(string idOrKey = "", string firstName = "", string lastName = "")
         {
             var returnValue = new List<PersonInfo>();
